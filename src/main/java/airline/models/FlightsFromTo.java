ac5be84d0f7a84a83ad0b6defb1cecaa6d3d38ca
prettyjose <prettyjosemec@gmail.com>
2017-08-31 15:03:28 +0530

    story 0 over with functionality. ui changes can be done

--- src/main/java/airline/models/FlightsFromTo.java
+++ src/main/java/airline/models/FlightsFromTo.java
@@ -2,4 +2,33 @@ package airline.models;
 
 public
 class FlightsFromTo {
+    String id, routeNo, flightNum, source, destination;
+
+    public
+    FlightsFromTo(){
+        this.flightNum = "";
+        this.source = "";
+        this.destination = "";
+    }
+    public
+    FlightsFromTo(String flightNum, String source, String destination) {
+        this.flightNum = flightNum;
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public
+    String getFlightNum() {
+        return flightNum;
+    }
+
+    public
+    String getSource() {
+        return source;
+    }
+
+    public
+    String getDestination() {
+        return destination;
+    }
 }
--- src/main/java/airline/models/Route.java
+++ src/main/java/airline/models/Route.java
@@ -2,4 +2,21 @@ package airline.models;
 
 public
 class Route {
+    String source, destination;
+
+    public
+    Route(String source, String destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public
+    String getSource() {
+        return source;
+    }
+
+    public
+    String getDestination() {
+        return destination;
+    }
 }
